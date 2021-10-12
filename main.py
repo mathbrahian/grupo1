bb2d0dc6216e9dae0b1a1499276f463b6d5844f4
Joanellav <90978702+Joanellav@users.noreply.github.com>
2021-10-11 21:14:57 -0500

    Cambio realizado por Joanella

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+import os
+from flask.helpers import flash
+from flask import Flask, render_template, url_for, redirect, jsonify
+
 
 app = Flask(__name__)
 
+app.secret_key = os.urandom(24)
 
-@app.route('/inicio_route')
+@app.route('/')
 def inicio_func():
     return render_template("inicio.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if(form.validate_on_submit()):
+        usuario = form.usuario.data
+        flash(f'Bienvenido(a), {usuario}')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080,debug=True)
