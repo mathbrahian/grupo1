c47efa9ef2a9bd5b727b76dd8b5a573f9fa1d8e7
mathbrahian <mathbrahian@gmail.com>
2021-10-11 22:23:08 -0500

    repartir trabajo

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Randyr
 @app.route('/')
 def inicio_func():
     return render_template("inicio.html")
+
+#Randyr
+@app.route('/servicio')
+def servicio_func():
+    return render_template("servicio.html")
+
+# Gustavo
+@app.route('/mision_vision')
+def mision_vision_func():
+    return render_template("mision_vision.html")
+
+# Joanella
+@app.route('/contacto')
+def contacto_func():
+    return render_template("contacto.html")
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8080,debug=True)
